17f21dce0f45c959c5bf12a06bfd3e97f3e76a24
sn00wden <matthieu2603@gmail.com>
2021-12-26 18:40:10 +0100

    6

---
+++ 6/6_1.py
@@ -0,0 +1,38 @@
+def clear_tab(text):
+    clear_tab_result = []
+    a_pass = ""
+    for line in range (0, len(text)):
+        
+        if len(text[line]) >1:
+            
+            a_pass += text[line].split('\n')[0]
+        else:
+            clear_tab_result.append(a_pass)
+            a_pass = ""
+    return clear_tab_result
+
+
+def line_counter(line):
+    temp = ""
+
+    for i in range(0, len(line)):
+        if(temp.find(line[i]) ==-1 and line[i] != ' ' ):
+            temp += line[i]
+        
+    # print(temp)
+    return len(temp)
+
+if __name__ == "__main__":
+    file = open('D:/Documents/RTU/Data_Structures/Advent_of_Code_2020/6/input.txt','r')
+
+    text = file.readlines()
+    # print(clear_tab(text))
+    main_counter = 0
+    cleared_text = clear_tab(text)
+    print(cleared_text)
+    # print(line_counter(cleared_text[len(cleared_text)-1]))
+
+    for i in range(0, len(cleared_text)):
+        main_counter += int(line_counter(cleared_text[i]))
+
+    print(main_counter)
---
+++ 6/6_2.py
@@ -0,0 +1,46 @@
+def clear_tab(text):
+    clear_tab_result = []
+    a_pass = ""
+    for line in range (0, len(text)):
+        
+        if len(text[line]) >1:
+            
+            a_pass += text[line]
+        else:
+            clear_tab_result.append(a_pass)
+            a_pass = ""
+        
+    for i in range(0, len(clear_tab_result)):
+        clear_tab_result[i]= clear_tab_result[i].split('\n')
+        clear_tab_result[i].pop()
+    return clear_tab_result
+
+
+def line_counter(grp):
+    temp = str(grp[0])
+    temp_2=""
+    for i in range(1, len(grp)):
+        for j in range(0, len(temp)):
+            index = grp[i].find(temp[j])
+            if( index != -1 ):
+                temp_2+=temp[j]
+        temp = temp_2
+        temp_2=""
+    # print(len(grp))
+    # print(temp)
+    return len(temp)
+
+if __name__ == "__main__":
+    file = open('D:/Documents/RTU/Data_Structures/Advent_of_Code_2020/6/input.txt','r')
+
+    text = file.readlines()
+    # print(clear_tab(text))
+    main_counter = 0
+    cleared_text = clear_tab(text)
+    # print(cleared_text)
+    # print(line_counter(cleared_text[len(cleared_text)-1]))
+
+    for i in range(0, len(cleared_text)):
+        main_counter += int(line_counter(cleared_text[i]))
+
+    print(main_counter)
